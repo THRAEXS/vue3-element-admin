c98f2bfaf4cd7cef7f3e138d8f0949930e492a6d
鬼王 <THRAEX@aliyun.com>
2022-02-27 22:54:42 +0800

    fix: semi

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import {
     createRouter,
     createWebHistory
-} from 'vue-router'
+} from 'vue-router';
 
 const routes = [
     { path: '/', component: _ => import('@/views/home.vue') },
     { path: '/login', component: _ => import('@/views/login.vue') },
     { path: '/about', component: _ => import('@/views/about.vue') }
-]
+];
 
 export default createRouter({
     history: createWebHistory(),
     routes
-})
+});
--- src/settings.js
+++ src/settings.js
@@ -1,4 +1,4 @@
 export default {
     title: 'vue3-admin-template',
     tokenKey: 'X-TOKEN'
-}
+};
